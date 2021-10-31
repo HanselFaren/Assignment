b0d1c12d62f16ce25ce5e3ff36d785fd4a91c736
hansel409 <91465998+hansel409@users.noreply.github.com>
2021-10-31 21:57:39 +0700

    Delete runway length.py

--- runway length.py
+++
@@ -1,6 +0,0 @@
-v = float(print("Enter the plane's take off speed in m/s : "))
-a = float(print("Enter the plane's acceleration in m/s**2 : "))
-
-length = v**2 / 2a
-
-print("The minimum runway length needed for this airplane is %2.4f"%length)
